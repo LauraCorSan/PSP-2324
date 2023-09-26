2a3221138cfe5d75e751c1840272494202f5919c
LauraCorSan <laura.corderosanchez.bp@gmail.com>
2023-09-26 20:31:30 +0200

    Añadir ej

---
+++ UT01/01/Ej3.c
@@ -0,0 +1,12 @@
+/*Números Primos
+
+Enunciado: Crea un programa que solicite al usuario ingresar un número entero positivo
+ y determine si es un número primo o no.
+
+Posibles Soluciones: 
+a. Utiliza un bucle for para verificar si el número es divisible por algún número 
+entre 2 y la raíz cuadrada del número. 
+b. Implementa una función que verifique la primalidad y llámala desde el programa 
+principal. 
+c. Utiliza la criba de Eratóstenes para generar una lista de números primos y 
+verifica si el número dado está en la lista.*/
---
+++ UT01/01/Ej4.c
@@ -0,0 +1,11 @@
+/*Cálculo de Factorial
+
+Enunciado: Desarrolla un programa que permita al usuario ingresar un número entero 
+no negativo y calcule su factorial. Asegúrate de manejar adecuadamente los casos de 
+entrada inválida.
+
+Posibles Soluciones: 
+a. Utiliza un bucle for para calcular el factorial. 
+b. Implementa una función recursiva para calcular el factorial. 
+c. Utiliza una matriz de factoriales precalculados para los números del 0 al N y
+ busca el valor en la matriz.*/
