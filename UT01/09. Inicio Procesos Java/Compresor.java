bb27742bd2e7952e21d20da19646a176bb6da03f
LauraCorSan <laura.corderosanchez.bp@gmail.com>
2023-10-30 19:18:41 +0100

    prueba 1

--- UT01/09. Inicio Procesos Java/Compresor.java
+++ UT01/09. Inicio Procesos Java/Compresor.java
@@ -1,7 +1,38 @@
+
+/*Investiga el comando tarde linux para crear backups de directorios.
+
+Crea un programa que reciba por parámetro varias rutas del sistema de fichero. El programa comprimirá cada una 
+de ellas con un proceso. Si se reciben 3 directorios creará 3 archivos comprimidos con el nombre de cada 
+directorio y la fecha YYYY-MM-DD.*/
+
+
 import java.io.IOException;
 
 public class Compresor {
+    public static final String FECHA = "_2023_10_30";
     public static void main(String[] args) throws IOException, InterruptedException{
+        String argumento=args[0];
+        String nombreArchivo=argumento.replace('/', '_')+FECHA;
+    
+        
+        String [] comands={
+            "tar",
+            "-czvf",
+            //args[0]+".tar.gz",
+            //pasarle el nombre 
+            nombreArchivo,
+            args[0]
+        };
+
+        //tar -czvf nombre-nuevo directorio
+
+        ProcessBuilder pb= new ProcessBuilder(comands);
+
+        Process process = pb.start();
+
+        pb.inheritIO();//imrpime lo que sea tambien en nuestro proceso
+
+        System.out.println("Proceso terminado");
         
     }
 }
